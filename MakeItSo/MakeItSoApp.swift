4aef9342047204c6efc42a61b9222deda75196ae
Azis R <alfatihstudi@gmail.com>
2023-07-19 21:03:29 +0700

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Azis Ramdhan on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

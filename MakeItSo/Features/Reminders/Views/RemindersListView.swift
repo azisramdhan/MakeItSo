79f925879119f493b4777b3a695c3da16e2cc2a3
Azis R <alfatihstudi@gmail.com>
2023-11-25 11:35:08 +0700

    Implementing Model, View, ViewModel (MVVM)

--- MakeItSo/Features/Reminders/Views/RemindersListView.swift
+++ MakeItSo/Features/Reminders/Views/RemindersListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RemindersListView.swift
 //  MakeItSo
 //
 //  Created by Azis Ramdhan on 19/07/23.
@@ -7,9 +7,9 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State
-    private var reminders = Reminder.samples
+struct RemindersListView: View {
+    @StateObject
+    private var viewModel = RemindersListViewModel()
     
     @State
     private var isAddReminderDialogPresented = false
@@ -19,7 +19,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        List($reminders) { $reminder in
+        List($viewModel.reminders) { $reminder in
             HStack {
                 Image(systemName: reminder.isCompleted
                       ? "largecircle.fill.circle"
@@ -27,7 +27,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
                 .onTapGesture {
-                    reminder.isCompleted.toggle()
+                    viewModel.toggleCompleted(reminder)
                 }
                 Text(reminder.title)
             }
@@ -46,7 +46,7 @@ struct ContentView: View {
         }
         .sheet(isPresented: $isAddReminderDialogPresented) {
             AddReminderView { reminder in
-                reminders.append(reminder)
+                viewModel.addReminder(reminder)
             }
         }
     }
@@ -54,7 +54,7 @@ struct ContentView: View {
 
 #Preview {
     NavigationStack {
-        ContentView()
+        RemindersListView()
             .navigationTitle("Reminders")
     }
 }
